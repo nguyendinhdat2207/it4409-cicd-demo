547c54539de4c62286872e8d3faad40600d05c51
Nguyễn Đình Đạt <nguyendinhdat22072005@gmail.com>
2026-05-27 10:47:31 +0700

    add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import errorHandler from './middlewares/errorMiddleware.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
